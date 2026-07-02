263e2bb805245ddc16be1bcdb032d89f9b01b406
Eduardo Cordeiro da Camara <eduardo.camara@icomp.ufam.edu.br>
2026-07-01 23:47:51 -0400

    impr(game-session): add view for game-session/create

--- src/controllers/game-session.ts
+++ src/controllers/game-session.ts
@@ -4,22 +4,19 @@ import type { CreateGameSessionDTO, UpdateGameSessionDTO } from "../types/game-s
 import gameSession from "../services/game-session.ts";
 
 const index = async (req: Request, res: Response) => {
-    const majors = await gameSession.getAllGameSessions();
-    res.render("game-session/index", { majors });
+    const gameSessions = await gameSession.getAllGameSessions();
+    res.render("game-session/index", { gameSessions });
 }
 
 const create = async (req: Request, res: Response) => {
     if (req.method === "GET") {
-        // res.render("game-session/create");
-        res.status(405).send("Method Not Allowed");
+        res.render("game-session/create");
     } else if (req.method === "POST") {
-        // res.send(JSON.stringify(req.body, null, 2));
-        const gameSessionData: CreateGameSessionDTO = req.body;
+        const gameSessionData: CreateGameSessionDTO = { ...req.body, score: Number(req.body.score) };
         try {
             await gameSession.createGameSession(gameSessionData);
             res.redirect("/game-session");
         } catch (error) {
-            console.error("Error creating game session:", error);
             res.status(500).send(error);
         }
     }
