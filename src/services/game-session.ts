446df797251164fa3b671822fd00730746f3fcc1
Eduardo Cordeiro da Camara <eduardo.camara@icomp.ufam.edu.br>
2026-07-01 23:15:04 -0400

    feat: implement game session management functions

---
+++ src/services/game-session.ts
@@ -0,0 +1,48 @@
+import prisma from "../utils/prismaClient.ts";
+import type {CreateGameSessionDTO, UpdateGameSessionDTO} from "../types/game-session.ts";
+
+
+function getAllGameSessions() {
+  return prisma.gameSession.findMany();
+}
+
+function getGameSessionsFromUserId(userId: string) {
+    return prisma.gameSession.findMany({
+        where: { userId } });
+}
+
+function getTopGameSessions(limit: number) {
+    return prisma.gameSession.groupBy({
+        by: ['userId'],
+        _max: { score: true },
+        orderBy: { _max: { score: 'desc' } },
+        take: limit,
+    });
+}
+
+function readGameSession(id: string) {
+    return prisma.gameSession.findUnique({
+        where: { id } });
+}
+
+function createGameSession(data: CreateGameSessionDTO) {
+    return prisma.gameSession.create({ data });
+}
+
+function updateGameSession(id: string, data: UpdateGameSessionDTO) {
+    return prisma.gameSession.update({ where: { id }, data });
+}
+
+function deleteGameSession(id: string) {
+    return prisma.gameSession.delete({ where: { id } });
+}
+
+export default {
+    getAllGameSessions,
+    getGameSessionsFromUserId,
+    getTopGameSessions,
+    readGameSession,
+    createGameSession,
+    updateGameSession,
+    deleteGameSession,
+}
